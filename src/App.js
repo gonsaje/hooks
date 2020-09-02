dccb0033f8fadcf6194810348afe602404c96b40
gonsaje <song.jaehyun@gmail.com>
2020-09-02 13:38:46 -0400

    added a click counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState('white')
+  const colors = ["white", "green", "pink", "beige"]
+  const randomTheme = colors[Math.floor(Math.random() * colors.length)]
+  console.log(randomTheme)
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Hooks</h1>
+      <div>
+        <h3>Click Counter</h3>
+        <p className="prompt">You clicked {count} times</p>
+        <button className="button" onClick={() => setCount(count + 1)}>
+          Click
+        </button>
+      </div>
+      <div>
+        <h3>Color Changer</h3>
+  <p>The current theme is {theme}</p>
+        <button className="button" onClick={() => setTheme(randomTheme)}>
+          Click
+        </button>
+      </div>
     </div>
   );
 }
